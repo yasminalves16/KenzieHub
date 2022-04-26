385c9f8f21f6e09cc1ad33e3e2804d0bce306024
YasminAlves16 <yasmin.alves16@outlook.com.br>
2022-04-25 23:19:10 -0300

    Feature- Structuring carried out pages

---
+++ src/Pages/Signup/index.jsx
@@ -0,0 +1,14 @@
+import { Container } from "./styles";
+
+
+const Signup = () => {
+
+    return (
+
+      <Container >
+        <p> Signup </p>
+      </Container>
+    );
+  }
+  
+  export default Signup
