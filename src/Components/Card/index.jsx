e21a216b05e708d9d42f3a16a454efb19539f902
YasminAlves16 <yasmin.alves16@outlook.com.br>
2022-04-27 10:17:24 -0300

    Feature, Style - Card

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -1,14 +1,38 @@
 import { Container } from "./styles"
+import { FaTrash, FaPen } from "react-icons/fa";
 
-const Card = ({tecnologias}) => {
+import Button from "../Button"
+import { toast } from "react-toastify";
 
-    console.log(tecnologias)
+
+import api from "../../Services/api"
+
+const Card = ({tecnologias, token, setModalEdit, setTecId}) => {
+
+    
+
+    const handleDelet = (id) =>{
+        api.delete(`users/techs/${id}`,{
+
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+        })
+        .then((response) => {
+            return toast.success("Tecnologia Excluida");
+        })
+    }
+
 
     return(
-        tecnologias.map((card, index) => {
-            return <Container key={index}>
+        tecnologias.map((card) => {
+            return <Container key={card.id}>
             <p>{card.title}</p>
             <p>{card.status}</p>
+            <div>
+            <Button onClick={() => {setModalEdit(true) ; setTecId(card.id)}}> <FaPen/></Button>
+            <Button onClick={() => handleDelet(card.id)}> <FaTrash/></Button>
+            </div>
             </Container>
         })
     )
--- src/Components/Card/styles.js
+++ src/Components/Card/styles.js
@@ -10,5 +10,28 @@ export const Container = styled.div`
     height: 60px;
     background: #121214;
     padding: 10px;
+    border-radius: 5px;
+    border: 2px solid transparent;
+
+        :hover{
+            border: 2px solid white;
+        }
+
+    p{
+        width: 80px;
+    }
     
+    button{
+        width:25px;
+        height: 30px;
+        margin:4px;
+    }
+
+    @media (max-width: 570px){
+
+        width: 70vw;
+        padding: 5px;
+
+    }
+
 `
